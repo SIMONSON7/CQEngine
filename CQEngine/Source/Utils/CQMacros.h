4f5c59e044157fc607ced6c2affd08e2e299068b
qiuhao <2454664898@qq.com>
2017-12-19 21:50:35 +0800

    update DEBUG

--- CQEngine/Source/Utils/CQMacros.h
+++ CQEngine/Source/Utils/CQMacros.h
@@ -7,8 +7,8 @@
 
 
 #if defined( _DEBUG ) || defined (__DEBUG__)
-#   ifndef CQ_DEBUG
-#       define CQ_DEBUG
+#   ifndef CQDEBUG
+#       define CQDEBUG
 #   endif
 #endif
 
@@ -21,7 +21,7 @@
 
 
 
-#if defined(CQ_DEBUG)
+#if defined(CQDEBUG)
 #   if defined(_MSC_VER)
 #       define CQASSERT( x )           if ( !((void)0,(x))) { __debugbreak(); }
 #   elif defined (ANDROID_NDK)
