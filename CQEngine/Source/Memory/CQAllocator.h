cc43d6e7212f01e076d8e4bf68fcee2543b9258a
qiuhao <2454664898@qq.com>
2017-12-26 14:10:54 +0800

    init mm

---
+++ CQEngine/Source/Memory/CQAllocator.h
@@ -0,0 +1,28 @@
+/*
+*
+*		qiu_hao		2017/12/25		04:11.pm
+*/
+#ifndef __CQALLOCATOR_H__
+#define __CQALLOCATOR_H__
+
+#include <new>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__CQALLOCATOR_H__*/
