508e3727e01ad7a6e766dfdaf1806cb4b32f43a9
qiuhao <2454664898@qq.com>
2017-12-28 01:02:35 +0800

    replace macros

--- CQEngine/Source/Memory/CQMemory.h
+++ CQEngine/Source/Memory/CQMemory.h
@@ -18,24 +18,26 @@ public:
 public:
 };
 
-
 NS_CQ_END
 
 //----------------------------------------------------------------------------
+
+#define DEFAULT_ALLOCATOR CQEngine::Memory::g_allocator
+
 namespace CQEngine { struct CQPlacemenNewTag {}; } // For placement new
 inline void* operator new(size_t, CQEngine::CQPlacemenNewTag, void *_ptr);
 inline void operator delete(void *, CQEngine::CQPlacemenNewTag, void *_ptr) throw();
 
-#define CQ_NEW(_type,...)					CQ_PLACEMENT_NEW(CQEngine::Memory::g_allocator->alloc(sizeof(_type)),_type,##__VA_ARGS__)
+#define CQ_NEW(_type,...)					CQ_PLACEMENT_NEW(DEFAULT_ALLOCATOR->alloc(sizeof(_type)),_type,##__VA_ARGS__)
 #define CQ_NEW0(_type,...)					CQ_NEW(_type,##__VA_ARGS__)
 #define CQ_NEW1(_ptr,_type,_size1,...)	\
-	_ptr = (_type*)CQEngine::Memory::g_allocator->alloc(_size1*sizeof(_type)); \
+	_ptr = (_type*)DEFAULT_ALLOCATOR->alloc(_size1*sizeof(_type)); \
 	_type *obj = _ptr; \
 	for (int i = 0 ; i < _size1; ++i ,++obj) \
 	{ \
 		CQ_PLACEMENT_NEW(obj,_type,##__VA_ARGS__); \
 	} \
-	CQEngine::Memory::g_allocator->setSize1(_size1);\
+	DEFAULT_ALLOCATOR->setSize1(_size1);\
 
 #define CQ_RAW_NEW(_type,...)				::new _type(##__VA_ARGS__);
 #define CQ_RAW_NEW0(_type,...)				CQ_RAW_NEW(_type,##__VA_ARGS__);
@@ -46,7 +48,7 @@ inline void operator delete(void *, CQEngine::CQPlacemenNewTag, void *_ptr) thro
 	if(_ptr) \
 	{ \
 		_ptr->~_type(); \
-		CQEngine::Memory::g_allocator->free(_ptr) ; \
+		DEFAULT_ALLOCATOR->free(_ptr); \
 		_ptr = nullptr; \
 	} \
 
@@ -54,14 +56,14 @@ inline void operator delete(void *, CQEngine::CQPlacemenNewTag, void *_ptr) thro
 #define CQ_DELETE1(_ptr,_type) \
 	if(_ptr) \
 	{\
-		size_t size = CQEngine::Memory::g_allocator->getSize1();\
+		size_t size = DEFAULT_ALLOCATOR->getSize1();\
 		_type *obj = _ptr; \
 		for(int i = 0; i < size; ++i ,++obj ) \
 		{\
 			obj->~_type(); \
 		}\
 	}\
-	CQEngine::Memory::g_allocator->free(_ptr); \
+	DEFAULT_ALLOCATOR->free(_ptr); \
 	_ptr = nullptr; \
 
 #define CQ_RAW_DELETE(_ptr)					do{ if(_ptr){::delete _ptr;_ptr=nullptr;} }while(0) 
