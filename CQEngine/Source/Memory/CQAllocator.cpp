e7395cc13274e0e12477a86623f17c8af471660a
qiuhao <2454664898@qq.com>
2017-12-28 17:19:01 +0800

    update DefaultAllocator

--- CQEngine/Source/Memory/CQAllocator.cpp
+++ CQEngine/Source/Memory/CQAllocator.cpp
@@ -1,4 +1,5 @@
 #include "CQAllocator.h"
+#include "CQDebug.h"
 
 USING_NS_CQ
 
@@ -9,11 +10,17 @@ DefaultAllocator::~DefaultAllocator() {}
 
 void* DefaultAllocator::alloc(size_t _size)
 {
+#ifdef CQDEBUG
+	dbgPrintf("\n [alloc]: \n FILE: %s \n LINE: %d \n", __FILE__, __LINE__);
+#endif
 	return malloc(_size);
 }
 
 void DefaultAllocator::free(void *_ptr)
 {
+#ifdef CQDEBUG
+	dbgPrintf("\n [free]: \n FILE: %s \n LINE: %d \n", __FILE__, __LINE__);
+#endif
 	CQASSERT(_ptr)
 	::free(_ptr);
 }
