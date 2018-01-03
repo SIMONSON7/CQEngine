dc5e814cb84523c3033d9f51304468cadad24235
qiuhao <2454664898@qq.com>
2018-01-03 21:13:18 +0800

    init CQMath

--- CQEngine/Source/Mathematics/CQMath.h
+++ CQEngine/Source/Mathematics/CQMath.h
@@ -1,1 +1,24 @@
-#pragma once
+/*
+*
+*		qiu_hao		2018/01/03		09:04.am
+*/
+#ifndef __CQMATH_H__
+#define __CQMATH_H__
+
+#include "CQMacros.h"
+
+NS_CQ_BEGIN
+
+extern const float kPI;
+extern const float kPI2;
+extern const float kPIHalf;
+
+
+
+
+
+
+NS_CQ_END
+
+#endif /* __CQMATH_H__ */
+
