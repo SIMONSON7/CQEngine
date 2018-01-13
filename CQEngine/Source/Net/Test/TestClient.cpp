c4d4169367e91a713ea9247cccc68161bbec9425
qiuhao <2454664898@qq.com>
2018-01-13 23:28:04 +0800

    update TestClient

--- CQEngine/Source/Net/Test/TestClient.cpp
+++ CQEngine/Source/Net/Test/TestClient.cpp
@@ -2,14 +2,57 @@
 	#define WIN32_LEAN_AND_MEAN
 	#include<windows.h>
 	#include<WinSock2.h>
+
+	#pragma comment(lib,"ws2_32.lib")
 #endif
 #include <stdio.h>
 
 
 int main(int argc,char *argv[])
 {
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET)
+	{
+		puts("CREATE SOCKET FAIL.");
+		return -1;
+	}
+	puts("INIT SOCKET SUCCESS.");
+
+	sockaddr_in sin = {};
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(4567);
+	sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	int ret = connect(sock, (sockaddr*)&sin, sizeof(sockaddr_in));
+	if (ret == SOCKET_ERROR)
+	{
+		printf("CONNECT SOCKET FAIL, RET_CODE = %d.\n",ret);
+		return -1;
+	}
+	puts("CONNECT SERVER SUCCESS.");
+
+	char cmdBuf[512] = {};
+	do
+	{
+		scanf("%s",cmdBuf);
+		if ((!strcmp(cmdBuf,"exit")) 
+			|| (!strcmp(cmdBuf,"EXIT")))
+		{
+			break;
+		}
 
 
 
+
+
+	} while (true);
+
+	closesocket(sock);
+	WSACleanup();
+	
+	puts("Bay.");
 	return 0;
 }
--- CQEngine/Source/Net/Test/TestServer.cpp
+++ CQEngine/Source/Net/Test/TestServer.cpp
@@ -1,11 +1,22 @@
 #if defined(_MSC_VER)
-#define WIN32_LEAN_AND_MEAN
-#include<windows.h>
-#include<WinSock2.h>
+	#define WIN32_LEAN_AND_MEAN
+	#include<windows.h>
+	#include<WinSock2.h>
+
+	#pragma comment(lib,"ws2_32.lib")
 #endif
 #include <stdio.h>
 
 
+
+
+
+
+
+
+
+
+#if 0
 int main(int argc, char *argv[])
 {
 
@@ -13,3 +24,4 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+#endif
