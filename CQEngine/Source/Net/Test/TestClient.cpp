e1bb328e8ef02e88695f2e49b7c848d8a336578a
qiuhao <2454664898@qq.com>
2018-01-13 21:40:35 +0800

    init Net test code.

---
+++ CQEngine/Source/Net/Test/TestClient.cpp
@@ -0,0 +1,15 @@
+#if defined(_MSC_VER)
+	#define WIN32_LEAN_AND_MEAN
+	#include<windows.h>
+	#include<WinSock2.h>
+#endif
+#include <stdio.h>
+
+
+int main(int argc,char *argv[])
+{
+
+
+
+	return 0;
+}
