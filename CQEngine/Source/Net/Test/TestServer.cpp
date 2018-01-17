5911f68aba123fe252c0384129ddc8b766983665
qiuhao <2454664898@qq.com>
2018-01-17 23:19:22 +0800

    server upgrade to Select Pattern.

--- CQEngine/Source/Net/Test/TestServer.cpp
+++ CQEngine/Source/Net/Test/TestServer.cpp
@@ -6,7 +6,33 @@
 	#pragma comment(lib,"ws2_32.lib")
 #endif
 #include <stdio.h>
+#include <vector>
 #include "proto.h"
+
+void work(SOCKET _cSock)
+{
+	Header head = {};
+	recv(_cSock, (char*)&head, sizeof(Header), 0);
+	printf("client package size = %d.\n", head.len_);
+
+	switch (head.code_)
+	{
+	case LOGIN:
+	{
+		LoginPackage login = {};
+		recv(_cSock, (char*)&login + sizeof(Header), head.len_ - sizeof(Header), 0);
+		printf("client login name = %s,pwd = %s.\n", login.name_, login.pwd_);
+
+		LoginRetPackage ret = {};
+		strcpy(ret.result_, "token is right!");
+		send(_cSock, (char*)&ret, sizeof(ret), 0);
+	}
+		return;
+	default:
+		puts("ERROR : CLIENT DATA INVAILD.");
+		return;
+	}
+}
 
 #if 1
 int main(int argc, char *argv[])
@@ -44,42 +70,54 @@ int main(int argc, char *argv[])
 	puts("SERVER LISTEN SUCCESS.");
 	puts("===== SERVER INIT SUCCESS ======");
 
+	std::vector<SOCKET> c_socks;
 	do
 	{
-		sockaddr_in cAddr = {};
-		int cAddrLen = sizeof(sockaddr_in);
-		SOCKET cSock = INVALID_SOCKET;
+		// SELECT //
+		fd_set read_fd;
+		fd_set write_fd;
+		fd_set exp_fd;
 
-		cSock = accept(sock, (sockaddr*)&cAddr, &cAddrLen);
-		if (cSock == INVALID_SOCKET)
+		FD_ZERO(&read_fd);
+		FD_ZERO(&write_fd);
+		FD_ZERO(&exp_fd);
+
+		FD_SET(sock, &read_fd);
+		FD_SET(sock, &write_fd);
+		FD_SET(sock, &exp_fd);
+
+		for (int i = 0 ; i < c_socks.size();++i)
 		{
-			puts("SERVER ACCEPT CLIENT ERROR.");
+			FD_SET(c_socks[i], &read_fd);
 		}
-		printf("SERVER ACCEPT THE CLIENT FROM :%s\n", inet_ntoa(cAddr.sin_addr));
 
-		Header head = {};
-		recv(cSock, (char*)&head, sizeof(Header), 0);
-		printf("client package size = %d.\n",head.len_);
-
-		switch (head.code_)
+		int ret = select(sock + 1,&read_fd,&write_fd,&exp_fd,nullptr);
+		if (ret < 0)
 		{
-			case LOGIN :
+			puts("SERVER SELECT ERROR.");
+			break;
+		}
+		if (FD_ISSET(sock, &read_fd))
+		{
+			FD_CLR(sock, &read_fd);
+
+			sockaddr_in cAddr = {};
+			int cAddrLen = sizeof(sockaddr_in);
+			SOCKET cSock = INVALID_SOCKET;
+
+			cSock = accept(sock, (sockaddr*)&cAddr, &cAddrLen);
+			if (cSock == INVALID_SOCKET)
 			{
-				LoginPackage login = {};
-				recv(cSock, (char*)&login + sizeof(Header), head.len_ - sizeof(Header), 0);
-				printf("client login name = %s,pwd = %s.\n",login.name_,login.pwd_);
-
-				LoginRetPackage ret = {};
-				strcpy(ret.result_, "token is right!");
-				send(cSock, (char*)&ret, sizeof(ret), 0);
+				puts("SERVER ACCEPT CLIENT ERROR.");
 			}
-				break;
-			default:
-				puts("ERROR : CLIENT DATA INVAILD.");
-				break;
+			c_socks.push_back(cSock);
+			printf("SERVER ACCEPT THE CLIENT FROM :%s\n", inet_ntoa(cAddr.sin_addr));
 		}
 
-
+		for (int i = 0; i < read_fd.fd_count; ++i)
+		{
+			work(read_fd.fd_array[i]);
+		}
 		
 	} while (true);
 
@@ -89,4 +127,4 @@ int main(int argc, char *argv[])
 	puts("Bay.");
 	return 0;
 }
-#endif
+#endif 
