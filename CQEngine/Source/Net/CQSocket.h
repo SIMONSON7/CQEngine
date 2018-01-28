a405f1fd35eea563898c4ed7d8075b1b710fdc53
qiuhao <2454664898@qq.com>
2018-01-28 23:07:41 +0800

    init CQSocket.

--- CQEngine/Source/Net/CQSocket.h
+++ CQEngine/Source/Net/CQSocket.h
@@ -5,7 +5,31 @@
 #ifndef __CQSOCKET_H__
 #define __CQSOCKET_H__
 
+#if defined(_MSC_VER)
+	#define WIN32_LEAN_AND_MEAN
+	#include<windows.h>
+	#include<WinSock2.h>
+
+	#pragma comment(lib,"ws2_32.lib")
+#else
+	#include <unistd.h>
+	#include <arpa/inet.h>
+	#include <string.h>
+
+	#define SOCKET int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif
+
+#include "CQMacros.h"
+
+NS_CQ_BEGIN
 
 
+
+
+
+
+NS_CQ_END
 
 #endif /*__CQSOCKET_H__*/
