81bc7157ba7ebbc2b97175b2ceae1eacd6f7889b
qiuhao <2454664898@qq.com>
2018-02-10 10:58:45 +0800

    add CQMemory comments.

--- CQEngine/Source/Memory/CQMemory.cpp
+++ CQEngine/Source/Memory/CQMemory.cpp
@@ -4,6 +4,9 @@ USING_NS_CQ
 
 AllocatorI* CQMemory::shareAllocator()
 {
+	// Not use dynamic alloc for s_allocator,
+	// then we No need to worry the dtor of s_allocator.
+	// "static" can guarantee the singleton of DefaultAllocator.
 	static DefaultAllocator s_allocator;
 	return &s_allocator;
 }
