048370076cce44e0f7f7ebe026bfc0ca3906f950
qiuhao <2454664898@qq.com>
2018-02-24 22:16:17 +0800

    update CQTuple interface.

--- CQEngine/Source/Mathematics/CQTuple.h
+++ CQEngine/Source/Mathematics/CQTuple.h
@@ -5,8 +5,40 @@
 #ifndef __CQTUPLE_H__
 #define __CQTUPLE_H__
 
+#include "CQMacros.h"
 
+NS_CQ_BEGIN
 
+template <size_t DIMENSION, typename TYPE>
+class Tuple
+{
+public:
+	Tuple();
+	Tuple(const Tuple& tuple);
+	~Tuple();
 
+	///
+	inline operator const TYPE* () const;
+	inline operator TYPE* ();
+	inline const TYPE& operator[] (int i) const;
+	inline TYPE& operator[] (int i);
+
+	///
+	Tuple& operator= (const Tuple& tuple);
+
+	///
+	bool operator== (const Tuple& tuple) const;
+	bool operator!= (const Tuple& tuple) const;
+	bool operator<  (const Tuple& tuple) const;
+	bool operator<= (const Tuple& tuple) const;
+	bool operator>  (const Tuple& tuple) const;
+	bool operator>= (const Tuple& tuple) const;
+
+protected:
+	TYPE mTuple[DIMENSION];
+
+};
+
+NS_CQ_END
 
 #endif /* __CQTUPLE_H__ */
