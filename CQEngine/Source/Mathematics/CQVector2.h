1e4814c014071b383fd9702ccfe530dff9d82028
qiuhao <2454664898@qq.com>
2018-02-28 22:09:56 +0800

    init CQVector2.

---
+++ CQEngine/Source/Mathematics/CQVector2.h
@@ -0,0 +1,22 @@
+/*
+*
+*		qiu_hao		2018/02/28		10:05.am
+*/
+#ifndef __CQVECTOR2_H__
+#define __CQVECTOR2_H__
+
+#include "CQTuple.h"
+
+NS_CQ_BEGIN
+
+template<typename Real>
+class Vector2 : Tuple<2, Real>
+{
+
+};
+
+#include "CQVector2.inl"
+
+NS_CQ_END
+
+#endif /* __CQVECTOR2_H__ */
