c3cac5b56a866ba1e4eb9649a4e0b0081159e2ef
qiuhao <2454664898@qq.com>
2018-03-08 15:35:22 +0800

    init texture load.

---
+++ CQEngine/Source/Utils/CQUtils.h
@@ -0,0 +1,23 @@
+/*
+*
+*		qiu_hao		2018/03/08		03.19.pm
+*/
+#ifndef __COUTILS_H__
+#define __COUTILS_H__
+
+#include "CQMacros.h"
+
+NS_CQ_BEGIN
+
+
+
+
+
+
+
+
+
+NS_CQ_END
+
+
+#endif /*__COUTILS_H__*/
