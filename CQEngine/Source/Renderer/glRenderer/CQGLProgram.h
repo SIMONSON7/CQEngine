818cffb00bc35bf6e409083750ea4170d8428a26
qiuhao <2454664898@qq.com>
2018-03-12 16:42:54 +0800

    init CQGLProgram.

---
+++ CQEngine/Source/Renderer/glRenderer/CQGLProgram.h
@@ -0,0 +1,19 @@
+/*
+*
+*		qiu_hao		2018/03/12		04:34.pm
+*/
+#ifndef __CQGLPROGRAM_H__
+#define __CQGLPROGRAM_H__
+
+#include "CQMacros.h"
+
+NS_CQ_BEGIN
+
+class CQGLProgram
+{
+
+};
+
+NS_CQ_END
+
+#endif /*__CQGLPROGRAM_H__*/
