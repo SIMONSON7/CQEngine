d2227cf1e644707d8486fce6fc1c2eadc0ff5ca3
qiuhao <2454664898@qq.com>
2018-04-19 22:27:21 +0800

    rough encapsulation texture interface.

--- CQEngine/Source/Renderer/glRenderer/CQGLTexture.cpp
+++ CQEngine/Source/Renderer/glRenderer/CQGLTexture.cpp
@@ -1,1 +1,37 @@
-#include "CQGLProgram.h"
+#include "CQGLTexture.h"
+#include "CQDebug.h"
+
+USING_NS_CQ;
+
+CQGLTexture::CQGLTexture(int _width, int _height, void *_data)
+	:
+	width_(_width),
+	height_(_height),
+	data_(_data)
+{
+	CQ_GLCHECK(glGenTextures(1, &textureID_));
+	CQ_GLCHECK(glBindTexture(GL_TEXTURE_2D, textureID_));
+	{
+		CQ_GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+		CQ_GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+		CQ_GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+		CQ_GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+		CQ_GLCHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width_, height_, 0, GL_RGB, GL_UNSIGNED_BYTE, data_));
+	}
+	CQ_GLCHECK(glBindTexture(GL_TEXTURE_2D, 0));
+}
+
+CQGLTexture::~CQGLTexture() {}
+
+void CQGLTexture::Bind()
+{
+	CQ_GLCHECK(glActiveTexture(GL_TEXTURE0));
+	CQ_GLCHECK(glBindTexture(GL_TEXTURE_2D, textureID_));
+}
+
+void CQGLTexture::Unbind()
+{
+	CQ_GLCHECK(glBindTexture(GL_TEXTURE_2D, 0));
+}
+
+
--- CQEngine/Source/Renderer/glRenderer/CQGLTexture.h
+++ CQEngine/Source/Renderer/glRenderer/CQGLTexture.h
@@ -5,17 +5,30 @@
 #ifndef __CQGLTEXTURE_H__
 #define __CQGLTEXTURE_H__
 
+#include <glad/glad.h>
 #include "CQMacros.h"
-#include "CQObject.h"
-#include <map>
+//#include "CQObject.h"
+//#include <map>
 
 NS_CQ_BEGIN
 
+// TODO £ºIt's just a rough package now.
+// Open more interfaces in the future.
 class CQGLTexture 
 {
-
-
-
+public:
+	CQGLTexture(int _width, int _height, void *_data);
+	virtual ~CQGLTexture();
+public:
+	const unsigned int GetTextureID() const;
+public:
+	void Bind();
+	void Unbind();
+private:
+	unsigned int textureID_;
+	int width_;
+	int height_;
+	void *data_;
 
 	//typedef std::map<TextureID, CQTexture*> TextureIDMap;
 	//static TextureIDMap s_textureIDMap;
