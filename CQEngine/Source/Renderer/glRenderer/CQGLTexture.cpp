6999028334eadcdaad246b5e1a0aa9bba01d8c56
qiuhao <2454664898@qq.com>
2018-04-19 23:26:47 +0800

    clean CQGLTexture ctor.

--- CQEngine/Source/Renderer/glRenderer/CQGLTexture.cpp
+++ CQEngine/Source/Renderer/glRenderer/CQGLTexture.cpp
@@ -11,8 +11,7 @@ CQGLTexture::CQGLTexture(int _width, int _height, void *_data)
 {
 	CQ_GLCHECK(glGenTextures(1, &textureID_));
 
-	CQ_GLCHECK(glActiveTexture(GL_TEXTURE0)); // option : for multi texture	
-	CQ_GLCHECK(glBindTexture(GL_TEXTURE_2D, textureID_));
+	Bind();
 	{
 		CQ_GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
 		CQ_GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
@@ -20,14 +19,14 @@ CQGLTexture::CQGLTexture(int _width, int _height, void *_data)
 		CQ_GLCHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 		CQ_GLCHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width_, height_, 0, GL_RGB, GL_UNSIGNED_BYTE, data_));
 	}
-	CQ_GLCHECK(glBindTexture(GL_TEXTURE_2D, 0));
+	Unbind();
 }
 
 CQGLTexture::~CQGLTexture() {}
 
 void CQGLTexture::Bind(int _stage)
 {
-	CQ_GLCHECK(glActiveTexture(GL_TEXTURE0 +_stage));
+	CQ_GLCHECK(glActiveTexture(GL_TEXTURE0 + _stage));
 	CQ_GLCHECK(glBindTexture(GL_TEXTURE_2D, textureID_));
 }
 
