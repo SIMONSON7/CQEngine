fff4be0157930ac04f1cb4d0a50760f551b909cc
qiuhao <2454664898@qq.com>
2018-08-28 23:54:53 +0800

    seq generator.

--- CQEngine/Source/Utils/CQAction.h
+++ CQEngine/Source/Utils/CQAction.h
@@ -2,3 +2,41 @@
 *
 *		qiu_hao		2018/08/27		21:53.pm
 */
+#ifndef __CQACTION_H__
+#define __CQACTION_H__
+
+template<size_t...idxs>
+struct Index
+{
+
+};
+
+template<size_t N, size_t...idxs>
+struct SeqGen : SeqGen<N - 1, N - 1, idxs...>
+{
+
+};
+
+template<size_t...idxs>
+struct SeqGen<0, idxs...> : Index<idxs...>
+{
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* __CQACTION_H__ */
