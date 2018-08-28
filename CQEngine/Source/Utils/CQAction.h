f9d75a8c42f2ec640683cf5c0b42f8ee41e0a639
qiuhao <2454664898@qq.com>
2018-08-29 00:20:17 +0800

    add CQAction.

--- CQEngine/Source/Utils/CQAction.h
+++ CQEngine/Source/Utils/CQAction.h
@@ -5,6 +5,13 @@
 #ifndef __CQACTION_H__
 #define __CQACTION_H__
 
+#include <tuple>
+#include <functional>
+#include <type_traits>
+#include "CQMacros.h"
+
+NS_CQ_BEGIN
+
 template<size_t...idxs>
 struct Index
 {
@@ -23,20 +30,30 @@ struct SeqGen<0, idxs...> : Index<idxs...>
 
 };
 
+class Action
+{
+	virtual void invoke() = 0;
+};
 
+template<typename Fn, typename...Args>
+class CQAction : public Action
+{
+public:
+	CQAction(const Fn&& _f, Args&&... _args);
 
+	virtual void invoke();
 
+private:
+	template<typename...args>
+	void __run(std::tuple<args...>& _tp);
 
+private:
+	Fn f_;
+	std::tuple<Args...> parms_;
+};
 
+#include "CQAction.inl"
 
-
-
-
-
-
-
-
-
-
+NS_CQ_END
 
 #endif /* __CQACTION_H__ */
--- CQEngine/Source/Utils/CQAction.inl
+++ CQEngine/Source/Utils/CQAction.inl
@@ -1,1 +1,13 @@
+template<typename Fn, typename...Args>
+CQAction<Fn, Args...>::CQAction(const Fn&& _f, Args&&... _args)
+{
+	f_ = std::forward<Fn>(_f);
+	parms_ = std::tuple<std::decay_t<Args>...>(std::forward<Args>(_args)...);
+}
+
+template<typename Fn, typename...Args>
+void CQAction<Fn, Args...>::invoke()
+{
+	__run(parms_);
+}
 
