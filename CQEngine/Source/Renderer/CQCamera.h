7c2ed7de8ff4645a190c5fae024a7abe6202498f
qiuhao <2454664898@qq.com>
2018-09-16 23:07:52 +0800

    init CQCamera.h

---
+++ CQEngine/Source/Renderer/CQCamera.h
@@ -0,0 +1,25 @@
+//*****************************************************************************
+//
+//! \file CQCamera.h
+//! \brief 
+//! \author qiu_hao
+//! \date 2018/09/16 11:03.pm
+//
+//*****************************************************************************
+
+#ifndef __CQCAMERA_H__
+#define __CQCAMERA_H__
+
+#include "CQMacros.h"
+
+NS_CQ_BEGIN
+
+class CQCamera
+{
+
+};
+
+NS_CQ_END
+
+#endif /* __CQCAMERA_H__ */
+
