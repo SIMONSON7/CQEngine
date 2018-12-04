bb28989c8d01ab9d7fb8df66fe5d2579e20de467
qiuhao <2454664898@qq.com>
2018-12-04 22:46:49 +0800

    init CQMesh.

--- CQEngine/Source/Renderer/CQMesh.cpp
+++ CQEngine/Source/Renderer/CQMesh.cpp
@@ -1,1 +1,34 @@
+#include "CQBuffer.h"
 #include "CQMesh.h"
+
+USING_NS_CQ
+
+CQMesh::CQMesh()
+{
+
+}
+
+CQMesh::CQMesh(CQVertexBuffer* _vb, CQIndexBuffer* _ib)
+{
+
+}
+
+CQMesh::CQMesh(const CQMesh & other)
+{
+
+}
+
+CQMesh& CQMesh::operator=(const CQMesh &other)
+{
+	return *this;
+}
+
+CQMesh::~CQMesh()
+{
+
+}
+
+std::string CQMesh::getName() const
+{
+	return "Mesh";
+}
