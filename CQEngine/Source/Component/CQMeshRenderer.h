495befbb5647acdd00eb5eadc49a59d9659b5293
qiuhao <2454664898@qq.com>
2019-01-14 23:28:13 +0800

    init CQMeshRenderer.

---
+++ CQEngine/Source/Component/CQMeshRenderer.h
@@ -0,0 +1,25 @@
+//*****************************************************************************
+//
+//! \file CQMeshRenderer.h
+//! \brief 
+//! \author qiu_hao
+//! \date 2018/11/08 10:25.pm
+//
+//*****************************************************************************
+
+#ifndef __CQMESHRENDERER_H__
+#define __CQMESHRENDERER_H__
+
+#include "CQComponent.h"
+#include "CQMath.h"
+
+NS_CQ_BEGIN
+
+class CQMeshRenderer : public CQComponent
+{
+
+};
+
+NS_CQ_END
+
+#endif /* __CQMESHRENDERER_H__ */
