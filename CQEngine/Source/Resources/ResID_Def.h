7a79091f6a19ddc5e9ad92fb8cd7074e6ae5b739
qiuhao <2454664898@qq.com>
2019-01-25 17:01:26 +0800

    add ResIDID_Def.

---
+++ CQEngine/Source/Resources/ResID_Def.h
@@ -0,0 +1,37 @@
+//*****************************************************************************
+//
+//! \file ResID_Def.h
+//! \brief 
+//! \author qiu_hao
+//! \date 2019/01/25 04:54.pm
+//
+//*****************************************************************************
+#ifndef __RESID_DEF__
+#define __RESID_DEF__
+
+#include "CQMacros.h"
+#include "ResID.h"
+
+NS_CQ_BEGIN
+
+class ResIDDef
+{
+public:
+	static const ResID DEF_VERTEX_SHADER;
+	static const ResID DEF_FRAGMENT_SHADER;
+
+public:
+	static const ResID HELLOWORLD_WALL_TEX;
+
+
+
+
+
+
+
+
+};
+
+NS_CQ_END
+
+#endif /* __RESID_DEF__ */
