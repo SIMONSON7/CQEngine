245d6270fbdf54e13276036ecba0838fce0cf140
qiuhao <2454664898@qq.com>
2019-02-11 14:51:29 +0800

    init CQMaterial.

---
+++ CQEngine/Source/Renderer/CQMaterial.h
@@ -0,0 +1,27 @@
+//*****************************************************************************
+//
+//! \file CQMaterial.h
+//! \brief 
+//! \author qiu_hao
+//! \date 2019/02/11 02:22.pm
+//
+//*****************************************************************************
+#ifndef __CQMATERIAL_H__
+#define __CQMATERIAL_H__
+
+#include "CQMacros.h"
+
+NS_CQ_BEGIN
+
+class CQMaterial
+{
+
+};
+
+NS_CQ_END
+
+#endif /* __CQMATERIAL_H__ */
+
+
+
+
