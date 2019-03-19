6fcfa56e8199dfc844a85916d0a2379031eecb4e
qiuhao <2454664898@qq.com>
2019-03-19 16:32:19 +0800

    init CQBevParallel.

---
+++ CQEngine/Source/AI/BehaviorTree/CQBevParallel.cpp
@@ -0,0 +1,18 @@
+#include "CQBevParallel.h"
+
+USING_NS_CQ;
+
+bool CQBevParallel::doEvaluate(const BevInParam &)
+{
+
+}
+
+void CQBevParallel::doTransition(const BevInParam &)
+{
+
+}
+
+BevRunningStatus CQBevParallel::doTick(const BevInParam &, BevOutParam &)
+{
+
+}
--- CQEngine/Source/AI/BehaviorTree/CQBevParallel.h
+++ CQEngine/Source/AI/BehaviorTree/CQBevParallel.h
@@ -1,1 +1,50 @@
-#pragma once
+//*****************************************************************************
+//
+//! \file CQBevParallel.h
+//! \brief Refer to https://github.com/FinneyTang/TsiU_AIToolkit_CSharp
+//! \author qiu_hao
+//! \date 2019/03/19 04:23.pm
+//
+//*****************************************************************************
+#ifndef __CQBEVPARALLEL_H__
+#define __CQBEVPARALLEL_H__
+
+#include <vector>
+#include "CQBevNode.h"
+
+NS_CQ_BEGIN
+
+enum class ParallelFinishCondition
+{
+	AND,
+	OR,
+};
+
+class CQBevParallel : public CQBevNode
+{
+public:
+	CQBevParallel(CQBevNode * _parent, CQBevPrecondition * _precondition);
+
+	virtual ~CQBevParallel() = default;
+
+public:
+	virtual bool doEvaluate(const BevInParam &);
+
+	virtual void doTransition(const BevInParam &);
+
+	virtual BevRunningStatus doTick(const BevInParam &, BevOutParam &);
+
+public:
+	inline void setFinishCondition(ParallelFinishCondition _condition) { finishCondition_ = _condition; }
+
+	inline ParallelFinishCondition getFinishCondition() const { return finishCondition_; }
+
+private:
+	ParallelFinishCondition finishCondition_;
+
+	std::vector<BevRunningStatus> childRunningStatus_;
+};
+
+NS_CQ_END
+
+#endif /* __CQBEVPARALLEL_H__ */
