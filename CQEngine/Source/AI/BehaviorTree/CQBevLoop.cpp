cd548da3c6a13cf2dfc48f43f120a3b133f7a8bc
qiuhao <2454664898@qq.com>
2019-03-26 23:13:21 +0800

    finish CQBevLoop.

--- CQEngine/Source/AI/BehaviorTree/CQBevLoop.cpp
+++ CQEngine/Source/AI/BehaviorTree/CQBevLoop.cpp
@@ -2,3 +2,62 @@
 
 USING_NS_CQ
 
+CQBevLoop::CQBevLoop(CQBevNode * _parent, CQBevPrecondition * _precondition, int _loopCnt = INFINITY_LOOP)
+	:
+	CQBevNode(_parent, _precondition),
+	loopCnt_(_loopCnt),
+	curLoopCnt_(0)
+{
+
+}
+
+bool CQBevLoop::doEvaluate(const BevInParam & _input)
+{
+	bool checkLoopCnt = (loopCnt_ == INFINITY_LOOP ||
+						curLoopCnt_ < loopCnt_);
+	if (!checkLoopCnt)
+	{
+		return false;
+	}
+
+	auto target = *children_.begin();
+	if (target)
+	{
+		return target->evaluate(_input);
+	}
+	return false;
+}
+
+void CQBevLoop::doTransition(const BevInParam & _input)
+{
+	auto target = *children_.begin();
+	if (target)
+	{
+		return target->transition(_input);
+	}
+	curLoopCnt_ = 0;
+}
+
+BevRunningStatus CQBevLoop::doTick(const BevInParam & _input, BevOutParam & _output)
+{
+	CQASSERT(children_.size());
+
+	BevRunningStatus status = BevRunningStatus::FINISH;
+	auto target = *children_.begin();
+	if (target)
+	{
+		status = target->tick(_input, _output);
+		if (status == BevRunningStatus::FINISH)
+		{
+			if (loopCnt_ != INFINITY_LOOP)
+			{
+				curLoopCnt_++;
+				if (curLoopCnt_ < loopCnt_ || loopCnt_ == INFINITY_LOOP)
+				{
+					status = BevRunningStatus::EXECUTING;
+				}
+			}
+		}
+	}
+	return status;
+}
