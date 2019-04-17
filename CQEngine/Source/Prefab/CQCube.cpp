bf90aceb12efa3ebfb320198a5d210a50b42538d
qiuhao <2454664898@qq.com>
2019-04-17 16:08:47 +0800

    add CQCube prefab.

---
+++ CQEngine/Source/Prefab/CQCube.cpp
@@ -0,0 +1,1 @@
+#include "CQCube.h"
---
+++ CQEngine/Source/Prefab/CQCube.h
@@ -0,0 +1,23 @@
+//*****************************************************************************
+//
+//! \file CQCube.h
+//! \brief 
+//! \author qiu_hao
+//! \date 2019/04/17 04:05.pm
+//
+//*****************************************************************************
+#ifndef __CQCUBE_H__
+#define __CQCUBE_H__
+
+#include "CQPrefab.h"
+
+NS_CQ_BEGIN
+
+class CQCube : public CQPrefab
+{
+
+};
+
+NS_CQ_END
+
+#endif /* __CQCUBE_H__ */
