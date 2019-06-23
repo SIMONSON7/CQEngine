c1b41d7503de95d92cf539a98787d0a75c597e02
qiuhao <2454664898@qq.com>
2019-06-23 21:44:02 +0800

    add NormalTexScene.

---
+++ Examples/NormalTex/Source/NormalTexScene.h
@@ -0,0 +1,21 @@
+//*****************************************************************************
+//
+//! \file NormalTexScene.h
+//! \brief 
+//! \author qiu_hao
+//! \date 2019/06/23 09:39.pm
+//
+//*****************************************************************************
+#include "CQEngine.h"
+
+#ifndef __NORMALTEXSCENE_H__
+#define __NORMALTEXSCENE_H__
+
+USING_NS_CQ
+
+class NormalTexScene : public CQScene
+{
+
+};
+
+#endif /* __NORMALTEXSCENE_H__ */
